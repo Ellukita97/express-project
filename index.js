1f9eca155e4e5b24ea35f076bec28b4919b61442
Ellukita97 <ebengarden@gmail.com>
2023-09-23 23:48:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { PORT } from "./config.js";
+import tasksRoutes from "./routes/tasks.routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(tasksRoutes);
+
+app.listen(PORT);
+console.log(`Servidor en el puerto ${PORT}`);
